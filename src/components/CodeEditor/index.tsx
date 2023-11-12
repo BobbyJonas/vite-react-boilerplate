7f99a09961dc77a2320003f96e30bfb5008a1b08
Gavin <gewenbo@vip.qq.com>
2023-11-12 23:10:29 +0800

    chore: fix

--- src/components/CodeEditor/index.tsx
+++ src/components/CodeEditor/index.tsx
@@ -1,9 +1,9 @@
 /* eslint-disable no-prototype-builtins */
-import React, { FC, useCallback, useEffect, useMemo, useRef } from 'react';
+import React, { FC, useCallback, useEffect, useRef } from 'react';
 import cs from 'classnames';
 
+import { languages, type editor } from 'monaco-editor';
 import Editor, { DiffEditor, useMonaco, loader, Monaco } from '@monaco-editor/react';
-import { languages } from 'monaco-editor';
 
 import typesRaw from './types.d.ts?raw';
 
@@ -20,9 +20,9 @@ const testAutoCompleteMap = {
 
 export interface ICodeEditorProps {}
 export const CodeEditor: FC<Readonly<ICodeEditorProps>> = () => {
-  const monaco = useMonaco();
+  const monaco = useMonaco() as Monaco;
 
-  const providerListRef = useRef<Array<Function>>([]);
+  const providerDisposeListRef = useRef<Array<Function>>([]);
 
   const transformObject2CustomCompletionProvider = useCallback(
     (obj: Record<string, any>): languages.CompletionItemProvider => {
@@ -53,36 +53,36 @@ export const CodeEditor: FC<Readonly<ICodeEditorProps>> = () => {
         // Function to generate autocompletion results
         provideCompletionItems: function (model, position, token) {
           // Split everything the user has typed on the current line up at each space, and only look at the last word
-          const last_chars = model.getValueInRange({
+          const lastChars = model.getValueInRange({
             startLineNumber: position.lineNumber,
             startColumn: 0,
             endLineNumber: position.lineNumber,
             endColumn: position.column,
           });
-          const words = last_chars.replace('\t', '').split(' ');
-          const active_typing = words[words.length - 1]; // What the user is currently typing (everything after the last space)
+          const words = lastChars.replace('\t', '').split(' ');
+          const activeTyping = words[words.length - 1]; // What the user is currently typing (everything after the last space)
 
           // If the last character typed is a period then we need to look at member objects of the obj object
-          const is_member = active_typing.charAt(active_typing.length - 1) == '.';
+          const isMember = activeTyping.charAt(activeTyping.length - 1) == '.';
 
           // Array of autocompletion results
           const result: languages.CompletionItem[] = [];
 
           // Used for generic handling between member and non-member objects
-          let last_token = obj;
+          let lastToken = obj;
           let prefix = '';
 
-          if (is_member) {
+          if (isMember) {
             // Is a member, get a list of all members, and the prefix
-            const parents = active_typing.substring(0, active_typing.length - 1).split('.');
-            last_token = obj[parents[0]];
+            const parents = activeTyping.substring(0, activeTyping.length - 1).split('.');
+            lastToken = obj[parents[0]];
             prefix = parents[0];
 
             // Loop through all the parents the current one will have (to generate prefix)
             for (let i = 1; i < parents.length; i++) {
-              if (last_token.hasOwnProperty(parents[i])) {
+              if (lastToken.hasOwnProperty(parents[i])) {
                 prefix += '.' + parents[i];
-                last_token = last_token[parents[i]];
+                lastToken = lastToken[parents[i]];
               } else {
                 // Not valid
                 return result;
@@ -93,33 +93,33 @@ export const CodeEditor: FC<Readonly<ICodeEditorProps>> = () => {
           }
 
           // Get all the child properties of the last token
-          for (const prop in last_token) {
-            // Do not show properites that begin with "__"
-            if (last_token.hasOwnProperty(prop) && !prop.startsWith('__')) {
+          for (const prop in lastToken) {
+            // Do not show properties that begin with "__"
+            if (lastToken.hasOwnProperty(prop) && !prop.startsWith('__')) {
               // Get the detail type (try-catch) incase object does not have prototype
               let details = '';
               try {
-                details = last_token[prop].__proto__.constructor.name;
+                details = lastToken[prop].__proto__.constructor.name;
               } catch (e) {
-                details = typeof last_token[prop];
+                details = typeof lastToken[prop];
               }
 
               // Create completion object
-              const to_push: languages.CompletionItem = {
+              const toPush: languages.CompletionItem = {
                 label: prefix + prop,
-                kind: getType(last_token[prop], is_member),
+                kind: getType(lastToken[prop], isMember),
                 detail: details,
                 insertText: prop,
               } as any;
 
               // Change insertText and documentation for functions
-              if (to_push.detail?.toLowerCase() == 'function') {
-                to_push.insertText += '()';
-                to_push.documentation = last_token[prop].toString().split('{')[0]; // Show function prototype in the documentation popup
+              if (toPush.detail?.toLowerCase() == 'function') {
+                toPush.insertText += '()';
+                toPush.documentation = lastToken[prop].toString().split('{')[0]; // Show function prototype in the documentation popup
               }
 
               // Add to final results
-              result.push(to_push);
+              result.push(toPush);
             }
           }
 
@@ -139,17 +139,20 @@ export const CodeEditor: FC<Readonly<ICodeEditorProps>> = () => {
     []
   );
 
-  const onFocus = useCallback(() => {
-    providerListRef.current = [
-      registerCompletionProvider(
-        monaco as any,
-        transformObject2CustomCompletionProvider(testAutoCompleteMap)
-      ),
-    ];
-  }, [monaco, registerCompletionProvider, transformObject2CustomCompletionProvider]);
+  const onFocus = useCallback(
+    (monaco: Monaco) => {
+      providerDisposeListRef.current = [
+        registerCompletionProvider(
+          monaco,
+          transformObject2CustomCompletionProvider(testAutoCompleteMap)
+        ),
+      ];
+    },
+    [registerCompletionProvider, transformObject2CustomCompletionProvider]
+  );
 
   const onBlur = useCallback(() => {
-    providerListRef.current.forEach((dispose) => {
+    providerDisposeListRef.current.forEach((dispose) => {
       dispose?.();
     });
   }, []);
@@ -157,8 +160,20 @@ export const CodeEditor: FC<Readonly<ICodeEditorProps>> = () => {
   useEffect(() => {
     monaco?.languages?.typescript.typescriptDefaults.addExtraLib(typesRaw, 'ts:utils.d.ts');
     monaco?.languages?.typescript.javascriptDefaults.addExtraLib(typesRaw, 'ts:utils.d.ts');
-    monaco && onFocus();
-  }, [monaco, monaco?.languages, onFocus]);
+  }, [monaco?.languages]);
+
+  const handleEditorMount = useCallback(
+    (editor: editor.IStandaloneCodeEditor, monaco: Monaco) => {
+      editor.onDidFocusEditorText(() => {
+        onFocus(monaco);
+      });
+
+      editor.onDidBlurEditorText(() => {
+        onBlur();
+      });
+    },
+    [onBlur, onFocus]
+  );
 
   return (
     <div>
@@ -167,8 +182,7 @@ export const CodeEditor: FC<Readonly<ICodeEditorProps>> = () => {
         className={cs('code-editor')}
         defaultLanguage="javascript"
         defaultValue="// hello world"
-        // onFocus={onFocus}
-        // onBlur={onBlur}
+        onMount={handleEditorMount}
       />
     </div>
   );
--- src/components/CodeEditor/types.d.ts
+++ src/components/CodeEditor/types.d.ts
@@ -1,9 +1,6 @@
+/* eslint-disable no-unused-vars */
+// TODO: 提示文档待补全
 interface UtilsType {
-  /**
-   * Calls a defined callback function on each element of an array, and returns an array that contains the results.
-   * @param callbackfn A function that accepts up to three arguments. The map method calls the callbackfn function one time for each element in the array.
-   * @param thisArg An object to which the this keyword can refer in the callbackfn function. If thisArg is omitted, undefined is used as the this value.
-   */
   a: (index: number) => void;
   b: () => void;
   hahah: () => void;
