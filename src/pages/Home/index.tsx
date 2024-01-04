3e9feae3f07d3d58aacfd042606b8c23e23f5a6a
葛文博 <gewenbo@kuaishou.com>
2024-01-04 14:27:24 +0800

    chore: fix

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,11 @@
 import React, { FC } from 'react';
+import CodeEditor from '@/components/CodeEditor';
 
 const HomePage: FC = () => {
   return (
     <div>
       <h1>HomePage</h1>
+      <CodeEditor></CodeEditor>
     </div>
   );
 };
